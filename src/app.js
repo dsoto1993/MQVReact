9490eea1cdfe1eaf8a7d53d14607a11eb59dc9ca
Daniel Soto <dasg.1993@gmail.com>
2019-04-08 14:16:35 -0500

    [MaquilladorV2] - setup webpack, babelrc

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+console.log('Hello');
+
+ReactDOM.render(<p>Maquillador virtual</p>, document.getElementById('app'));
+
